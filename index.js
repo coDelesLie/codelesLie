4bca713dd92cd1e677392968c9e2b4004ce5d76a
He Li <he.li@stargt.com.cn>
2016-01-14 15:11:53 +0800

    start

--- index.js
+++ index.js
@@ -3,30 +3,26 @@
  */
 "use strict";
 let express = require('express');
-let routes = require('./routes');
 let path = require('path');
-
 let favicon = require('serve-favicon');
 let session = require('express-session');
 let bodyParser = require('body-parser');
 let multer = require('multer');
 let errorHandler = require('errorhandler');
-
+let routes = require('./routes/index');
 let app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(methodOverride());
+app.set('view engine', 'ejs');
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(session({ resave: true,
     saveUninitialized: true,
     secret: 'uwotm8' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(multer());
+//app.use(multer());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
@@ -34,8 +30,10 @@ if ('development' == app.get('env')) {
     app.use(errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('', function (req,res) {
+    routes.loginin(req,res);
+});
+
 
 app.listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by user1016 on 2016/1/13.
+ */
+"use strict";
+class login{
+    constructor(){
+
+    }
+
+    loginin(req,res){
+        res.render('model',{});
+    }
+
+    logout(){
+        console.log(".................out")
+    }
+
+}
+
+module .exports = new login();
